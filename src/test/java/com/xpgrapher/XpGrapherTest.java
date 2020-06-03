7c40027d12301098965cc2ef63d68a357eb7ae09
cosmicquahog <cosmicquahog@gmail.com>
2020-06-02 21:47:50 -0400

    Rebasing plugin source files from origin\master

--- src/test/java/com/xpgrapher/XpGrapherTest.java
+++ src/test/java/com/xpgrapher/XpGrapherTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.xpgrapher;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class XpGrapherTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(XpGrapherPlugin.class);
 		RuneLite.main(args);
 	}
 }
