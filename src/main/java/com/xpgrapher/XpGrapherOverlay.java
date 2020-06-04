bdd14c5e216ad6939c50f8feedafce15f92da5e2
cosmicquahog <cosmicquahog@gmail.com>
2020-06-04 04:20:32 -0400

    Removed position selector. Made the overlay alt draggable.

--- src/main/java/com/xpgrapher/XpGrapherOverlay.java
+++ src/main/java/com/xpgrapher/XpGrapherOverlay.java
@@ -4,12 +4,14 @@ import com.google.inject.Inject;
 import net.runelite.api.Client;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
+import net.runelite.client.ui.overlay.OverlayPanel;
 import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.components.LayoutableRenderableEntity;
 
 import java.awt.*;
 import java.util.ArrayList;
 
-public class XpGrapherOverlay extends Overlay {
+public class XpGrapherOverlay extends OverlayPanel {
 
     private final Client client;
     private XpGrapherPlugin grapherPlugin;
@@ -30,27 +32,59 @@ public class XpGrapherOverlay extends Overlay {
     @Override
     public Dimension render(Graphics2D graphics)
     {
-        graphics.setColor(Color.GRAY);
-        graphics.drawRect(0, 0, grapherPlugin.width, grapherPlugin.height);
+        LayoutableRenderableEntity graphEntity = new LayoutableRenderableEntity() {
+            @Override
+            public Dimension render(Graphics2D graphics) {
+                graphics.setColor(Color.GRAY);
+                graphics.drawRect(0, 0, grapherPlugin.width, grapherPlugin.height);
 
-        graphics.setColor(Color.WHITE);
-        int numberOfPoints = grapherPlugin.graphPoints.size();
-        int oldX = -1;
-        int oldY = -1;
-        for (int i = 0; i < numberOfPoints; i++) {
-            Integer[] point = grapherPlugin.graphPoints.get(i);
-            int x = point[0];
-            int y = point[1];
-            if (y < grapherPlugin.height && y >= 0)
-                graphics.drawLine(x, y, x, y);
-            if (oldX != -1 && oldY != -1) {
-                graphics.drawLine(oldX+1, oldY, x, y);
+                graphics.setColor(Color.WHITE);
+                int numberOfPoints = grapherPlugin.graphPoints.size();
+                int oldX = -1;
+                int oldY = -1;
+                for (int i = 0; i < numberOfPoints; i++) {
+                    Integer[] point = grapherPlugin.graphPoints.get(i);
+                    int x = point[0];
+                    int y = point[1];
+                    if (y < grapherPlugin.height && y >= 0)
+                        graphics.drawLine(x, y, x, y);
+                    if (oldX != -1 && oldY != -1) {
+                        graphics.drawLine(oldX+1, oldY, x, y);
+                    }
+
+                    oldX = x;
+                    oldY = y;
+                }
+                return new Dimension(grapherPlugin.width, grapherPlugin.height);
             }
 
-            oldX = x;
-            oldY = y;
-        }
-        return null;
+            @Override
+            public Rectangle getBounds() {
+                return new Rectangle(grapherPlugin.width, grapherPlugin.height);
+            }
+
+            @Override
+            public void setPreferredLocation(Point position) {
+                return;
+            }
+
+            @Override
+            public void setPreferredSize(Dimension dimension) {
+                return;
+            }
+        };
+
+
+
+        panelComponent.getChildren().add(graphEntity);
+        panelComponent.setBackgroundColor(new Color(0, 0, 0, 0));
+
+
+
+
+
+
+        return super.render(graphics);
     }
 
 }
